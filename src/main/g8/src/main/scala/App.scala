db4b54a3560c6fb22392b6898bb674c218d577f5
softprops <d.tangren@gmail.com>
2011-05-24 22:41:16 -0400

    update example with a netty http plan

--- src/main/g8/src/main/scala/App.scala
+++ src/main/g8/src/main/scala/App.scala
@@ -2,24 +2,33 @@ object App {
   import unfiltered.netty.websockets._
   import unfiltered.util.Browser
   import scala.collection.mutable.ListBuffer
+  import unfiltered.response.ResponseString
 
   def main(args: Array[String]) {
     (1 to $socket_connections$).foreach { i =>
       Browser.open("file://%s" format getClass.getResource("client.html").getFile)
     }
     val sockets = new ListBuffer[WebSocket]()
-    WebSocketServer("/", $websocket_port$) {
-      case Open(s) =>
-        sockets.foreach(_.send("socket %s joined" format sockets.size + 1))
-        sockets + s
-        s.send("hola!")
-      case Message(s, Text(msg)) =>
-        sockets.foreach(_.send(msg))
-      case Close(s) =>
-        sockets - s
-        println("closed socket %s" format(s))
-      case Error(s, e) =>
-        println("error occured %s" format e.getMessage)
-    } run
+
+    unfiltered.netty.Http($websocket_port$).handler(unfiltered.netty.websockets.Planify({
+      case _ =>{
+          case Open(s) =>
+            sockets.foreach(_.send("socket %s joined" format sockets.size + 1))
+            sockets + s
+            s.send("hola!")
+          case Message(s, Text(msg)) =>
+            sockets.foreach(_.send(msg))
+          case Close(s) =>
+            sockets - s
+            println("closed socket %s" format(s))
+          case Error(s, e) =>
+            println("error occured %s" format e.getMessage)
+      }
+    })
+     .onPass(_.sendUpstream(_))
+    .handler(unfiltered.netty.cycle.Planify{
+      case _ => ResponseString("not a websocket")
+    })
+    .run
   }
 }
