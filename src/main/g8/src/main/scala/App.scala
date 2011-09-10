0e7d8e8afd96f2a8bb49df9b32dda09c52157abe
softprops <d.tangren@gmail.com>
2011-09-09 23:26:34 -0400

    a very simple protocol

--- src/main/g8/src/main/scala/App.scala
+++ src/main/g8/src/main/scala/App.scala
@@ -1,38 +1,38 @@
 object App {
   import unfiltered.netty.websockets._
   import unfiltered.util._
-  import scala.collection.mutable.ListBuffer
+  import scala.collection.mutable.ConcurrentMap
   import unfiltered.response.ResponseString
 
   def main(args: Array[String]) {
+    import scala.collection.JavaConversions._
+    val sockets: ConcurrentMap[Int, WebSocket] =
+      new java.util.concurrent.ConcurrentHashMap[Int, WebSocket]
 
-    val sockets = new ListBuffer[WebSocket]()
-
+    def notify(msg: String) = sockets.values.foreach(_.send(msg))
     unfiltered.netty.Http($websocket_port$).handler(unfiltered.netty.websockets.Planify({
-      case r =>
-        println("websockify %s".format(r));
-        ({
-          case Open(s) =>
-            sockets.foreach(_.send("socket %s joined" format sockets.size + 1))
-            sockets + s
-            s.send("hola!")
-          case Message(s, Text(msg)) =>
-            sockets.foreach(_.send(msg))
-          case Close(s) =>
-            sockets - s
-            println("closed socket %s" format(s))
-          case Error(s, e) =>
-            println("error occured %s" format e.getMessage)
-      })
+      case _ => {
+        case Open(s) =>
+          notify("%s|joined" format s.channel.getId)
+          sockets += (s.channel.getId.intValue -> s)
+          s.send("sys|hola!")
+        case Message(s, Text(msg)) =>
+          notify("%s|%s" format(s.channel.getId, msg))
+        case Close(s) =>
+          sockets -= s.channel.getId.intValue
+          notify("%s|left" format s.channel.getId)
+        case Error(s, e) =>
+          e.printStackTrace
+      }
     })
     .onPass(_.sendUpstream(_)))
     .handler(unfiltered.netty.cycle.Planify{
       case _ => ResponseString("not a websocket")
     })
-    .run({s =>
+    .run {s =>
        (1 to $socket_connections$).foreach { i =>
-         Browser.open("file://%s" format getClass.getResource("client.html").getFile)
+         Browser.open("file://%s" format(App.getClass.getResource("client.html").getFile))
        }
-     })
+     }
   }
 }
